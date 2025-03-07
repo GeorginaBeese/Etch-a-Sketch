d298a0713011b8772e6337a33f6154b26581e5df
GeorginaBeese <georgina_beese@outlook.com>
2025-03-07 08:44:45 +0000

    User created Grid with Hover effect

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Plan of Action:
  - create a div container in the html file
  - using java script create a 16x16 grid of square divs
  - use flexbox to make the divs appear as a grid
+
+ - Ask the user for the number of squares per side for the new grid. (max imput 100)
+ - remove any old grid and add a new one with that number of sides.
+ 
+
  
--- script.js
+++ script.js
@@ -2,16 +2,38 @@
 // Create a 16 x 16 grid
 
 let gridContainer = document.querySelector("#container");
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-for (let i = 0; i <= 255; i++) {
-    const square = document.createElement("div");
-    square.id = "square";
-    gridContainer.appendChild(square)
-    
-    square.addEventListener('mouseover', function () {
-        square.style.backgroundColor = 'red';
-    });
+function createGrid(x) {
+
+    // Removing any existing grid
+
+    const square = document.querySelector("#square");
+    if (square) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+          }
+        }
+
+    // Creating a new grid
+
+    for (let i = 1; i <= (x*x); i++) {
+        const square = document.createElement("div");
+        square.id = "square";
+        gridContainer.appendChild(square)
+        square.addEventListener('mouseover', function () {
+            square.style.backgroundColor = 'red';
+        });
+    } 
+    const root = document.documentElement;
+    const newWidth = `${100/x}%`
+    root.style.setProperty('--div-width', newWidth);   
 }
 
-
-
+button.addEventListener('click', () => {
+    const dimension = input.value;
+    input.value = '';
+    input.focus();
+    createGrid(dimension)
+})
